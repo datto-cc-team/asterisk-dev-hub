8ab18853dfd8a8fd6cff721b2f77d7475f440617
ddushok <ddushok@datto.com>
2019-10-09 17:10:41 -0400

    fix asterisk starting

--- docker/asterisk/config/run.php
+++ docker/asterisk/config/run.php
@@ -16,16 +16,19 @@ exec('sed -i "s/@@ASTERISK_SSH_PORT@@/$ASTERISK_SSH_PORT/g" /etc/ssh/sshd_config
 exec('service ssh stop');
 exec('service ssh start');
 
-// start asterisk
 /**
  * start asterisk
  *
  * odbc connection was not starting (despite setting "depends on" in docker-compose to force the db to start first)
  * adding a sleep of 5 seconds before starting asterisk solved this issue
+ *
+ * Sometime starting asterisk with service results in error "Starting Asterisk PBX: Unable to install capabilities."
+ * Using command asterisk starts without errors.  If asterisk was already running, a warning is displayed.
  */
 sleep(5);
-exec('service asterisk stop');
-exec('service asterisk start');
+exec('asterisk');
+//exec('service asterisk stop');
+//exec('service asterisk start');
 
 /**
  * watch files and dirs for changes
